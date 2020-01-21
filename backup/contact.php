9645de4f34f3cbdd9c59292f7e47aec3410614b3
Nick Kharchenko <57947402+227714@users.noreply.github.com>
2020-01-21 13:59:54 +0100

    Add files via upload

---
+++ backup/contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css";>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bakkerij</title>
+</head>
+<body>
+        <div id="header">
+            <h1>Het Oerbroodje</h1>
+        </div>
+            <ul id="nav">
+          <li><a class="nav-box" href="index.html">Home</a></li>
+          <li><a class="nav-box" href="broodjevdd.html">Broodje van de dag</a></li>
+          <li><a class="nav-box" href="belegdebroodjes.html">Belegde broodjes</a></li>
+          <li><a class="nav-box" href="gebak.html">Gebak</a></li>
+          <li><a class="nav-box" href="overons.html">Over ons</a></li>
+          <li><a class="nav-box" id="active" href="contact.php">Contact</a></li>
+            </ul>
+        <div class="main">
+<br>
+            <div class="main">
+                <h1 class="Page-titel">Contact</h1>
+                <div id="tekst">
+                  <p class="Alinea-text">
+                    <ul>
+                          <li><?php echo "Telefoon nmr: +31 06 0879 634800" ?> </li>
+                          <li><?php echo "Mail: coorperatie_sareon@outlook.com" ?> </li>
+                          <li><?php echo "Adress: Plagiaat 21 9725 SR Bergschedriel" ?></li>
+                    </ul>
+                <img id="imgRoom" src="IMG/Map" alt="Map">
+                </div>
+<br>
+        <div id="footer">
+                      Copyright &copy;
+        </div>
+    </div>
+  </div>
+</body>
+<script src="script.js"></script>
+</html>
+
