ab128dbdaed3e6fac269d75d53f5550b890edef9
Sem217201 <57891709+Sem217201@users.noreply.github.com>
2020-02-04 14:13:24 +0100

    Add files via upload

---
+++ website scrabs/contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css";>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bakkerij</title>
+</head>
+<body>
+        <div id="header">
+            <h1>Jan Jansen</h1>
+        </div>
+    <!--    de navigatie   -->
+        <ul>
+          <li><a class="nav-box" href="index.html">Home</a></li>
+          <li><a class="nav-box" href="broodjevdd.html">Broodje van de dag</a></li>
+          <li><a class="nav-box" href="belegdebroodjes.html">Belegde broodjes</a></li>
+          <li><a class="nav-box" href="gebak.html">Gebak</a></li>
+          <li><a class="nav-box" href="overons.html">Over ons</a></li>
+          <li><a class="nav-box" id="active" href="contact.php">Contact</a></li>
+          <div style="clear: both;"></div>
+       </ul>
+        <div class="main">
+<br>
+            <div class="main">
+                <h1 class="Page-titel">Contact</h1>
+                <div id="tekst">
+                  <p class="Alinea-text">
+                    <ul>
+                          <li><?php echo "Telefoon nmr: +31 06-97835163" ?> </li>
+                          <li><?php echo "Mail: OmurVroege@jourrapide.com" ?> </li>
+                          <li><?php echo "Adress: In de Wolken 93 Amstelveen " ?></li>
+                    </ul>
+                <img id="imgRoom" src="IMG/map.png" alt="Map">
+                </div>
+<br>
+        <div id="footer">
+                      Copyright &copy;
+        </div>
+    </div>
+  </div>
+</body>
+<script src="script.js"></script>
+</html>
+
